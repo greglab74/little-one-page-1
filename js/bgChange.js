c8d8a9b3656036807cf56b1740031dca4de741cd
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-06-04 14:44:13 +0200

    changement mineurs

--- js/bgChange.js
+++ js/bgChange.js
@@ -4,20 +4,28 @@ function bgChange() {
     let i = 0;
 
     function changer() {
-
-
         if(i < images.length) {
+            //fondu(false, header);
             header.style.backgroundImage = `url('../img/${images[i]}')`;
+            //fondu(true, header);
             i++
         } else {
-            console.log('le else');
             i = 0;
         }
     }
 
     setInterval(changer, 3000);
 
-    changer();
+    function fondu(in_out, cible) {
+        //à tester avec des console log car je n'ai pas de résultat
+        if(in_out) {
+            for(let j = 0 ; t <= 100 ; t++)
+                cible.style.opacity = t + '%';
+        } else {
+            for(let k = 0 ; t <= 100 ; t++)
+                cible.style.opacity = -t + '%';
+        }
+    }
 }
 
 
